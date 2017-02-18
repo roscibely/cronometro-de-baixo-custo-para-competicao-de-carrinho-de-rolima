4acc25df72b7f3616e823a6246883166976bb314
Ros Cibely <Ros Cibely>
2017-02-18 09:51:28 -0200

    Codigo utilizado no microcontrolador ao qual estava conectador o modulo Coordinator

---
+++ coordinator/coordinator.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#define SENSOR 5
+int partida = 1;
+int chegada = 0;
+int con = 2;
+SoftwareSerial mySerial(2,3); // RX, TX
+void setup() {
+  pinMode(SENSOR,INPUT);
+  mySerial.begin(9600);
+  Serial.begin(9600);
+}
+void loop() {
+  partida = mySerial.read();
+  chegada = digitalRead(SENSOR);
+  
+  if(partida == 0){
+    con = 1;
+    Serial.println(con);
+  }
+  else if(con==1 && chegada == 0){
+    con = 0;
+    Serial.println(con);
+    con = 2;
+  } 
+}
